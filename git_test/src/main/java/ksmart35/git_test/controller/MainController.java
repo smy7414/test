0c45b5d039abba34d973ab781ea6c6f2a6aadb6a
smy7414 <smy7414@naver.com>
2020-06-08 10:42:18 +0900

    [git_test] 인덱스화면 구현 file:  index.html, MainController.java detail : 인덱스 화면 폼 설계

---
+++ git_test/src/main/java/ksmart35/git_test/controller/MainController.java
@@ -0,0 +1,18 @@
+package ksmart35.git_test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("title", "ksmart35");
+		
+		return "index";
+		
+	}
+
+}
